4a4f4713edfa3ecdb91ceed84c16bcd68d636de7
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-03 11:51:18 +0300

    fix: unnecessary lines deleted

--- src/app/api/spotify/me/tracks/route.ts
+++ src/app/api/spotify/me/tracks/route.ts
@@ -6,7 +6,6 @@ export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
 
   const res = await fetch(`https://api.spotify.com/v1/me/tracks?limit=2&offset=1`, {
-    method: "GET",
     headers: {
       Authorization: `Bearer ${session?.access_token}`,
       "Content-Type": "application/json",
