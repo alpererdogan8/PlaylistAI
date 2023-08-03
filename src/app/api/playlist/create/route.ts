88a6df82240fe877390e210c40f184039a113323
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-03 16:16:29 +0300

    fix:fixed post issues with fetch

--- src/app/api/playlist/create/route.ts
+++ src/app/api/playlist/create/route.ts
@@ -1,19 +1,15 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
 
-type TRequest = { name: string; description: string; public: boolean };
-
 export async function POST(request: Request) {
-  const req: TRequest = await request.json();
-  const session = await getServerSession(authOptions);
-  const res = await fetch(`https://api.spotify.com/v1/users/${session?.user?.id}/playlists`, {
+  const req = await request.json();
+  const res = await fetch(`https://api.spotify.com/v1/users/${req?.session?.id}/playlists`, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${session?.access_token}`,
+      Authorization: `Bearer ${req?.session?.access_token}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(req),
+    body: JSON.stringify(req.body),
   });
+
   return NextResponse.json(await res.json());
 }
