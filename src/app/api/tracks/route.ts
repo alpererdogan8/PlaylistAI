f557566c9c0eca2fb935b3fedb3ae8dcc42e368b
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-03 16:46:22 +0300

    feat: path of api tracks changed

---
+++ src/app/api/tracks/route.ts
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../../auth/[...nextauth]/route";
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  const res = await fetch(`https://api.spotify.com/v1/me/tracks?limit=2&offset=1`, {
+    headers: {
+      Authorization: `Bearer ${session?.access_token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  return NextResponse.json(await res.json());
+}
