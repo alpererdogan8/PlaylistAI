3904cc1e0c10dc081b9913c3fa007aeadf84ed37
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-05 11:38:44 +0300

    fix: fixed incorrect authOptions path

--- src/app/api/tracks/route.ts
+++ src/app/api/tracks/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "../../../auth/[...nextauth]/route";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
