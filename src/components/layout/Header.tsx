a3586a069d33cd913be87e37c08201e254032fcf
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-05 08:14:34 +0300

    fix: line spaces cleared and user information shown after login

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import { Login } from "../LoginBtn";
+import { ProfileCard } from "../ProfileCard";
+import { Button } from "../ui/button";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export function Header() {
+export async function Header() {
+  const session = await getServerSession(authOptions);
+
   return (
     <header className="w-full">
       <nav className="w-full flex justify-between items-center px-[8px] md:px-[3rem] py-[2.75rem] text-black">
@@ -10,7 +16,7 @@ export function Header() {
           className=" text-2xl font-extrabold text-[2.3rem] -tracking-[0.055em]  lg:text-[2.813rem] w-full  padding lg:font-bold ">
           PLAYLIST AI
         </Link>
-        <Login>Sign in with Spotify</Login>
+        {session === null ? <Login>Sign in with Spotify</Login> : <ProfileCard />}
       </nav>
     </header>
   );
