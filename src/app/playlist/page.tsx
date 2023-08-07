1658792a958429a45c2a71c3e1ccd7e54e93d986
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-07 21:33:54 +0300

    feat: if not logged in return home redirect added

--- src/app/playlist/page.tsx
+++ src/app/playlist/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const getPlaylist = async ({ token }: { token: string }) => {
@@ -14,20 +15,27 @@ const getPlaylist = async ({ token }: { token: string }) => {
 };
 
 const Page = () => {
-  const { data, status, update } = useSession();
+  const { data, status, update } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/");
+    },
+  });
+
   const [myPlaylist, setMyPlaylist] = useState([]);
+
   useEffect(() => {
     async function fetchPlaylist() {
       const res = await getPlaylist({ token: data?.access_token! });
       setMyPlaylist(await res);
     }
     fetchPlaylist();
-  }, [data?.access_token!]);
+  }, [data?.access_token]);
 
   return (
     <>
       <pre className="whitespace-pre-wrap w-7/12">{JSON.stringify({ myPlaylist }, null, 2)}</pre>
-      {/* <pre className="whitespace-pre-wrap">{JSON.stringify({ data: data?.access_token }, null, 2)}</pre> */}
+      <pre className="whitespace-pre-wrap">{JSON.stringify({ data: data?.access_token }, null, 2)}</pre>
       <hr />
     </>
   );
