d335693ba03ff3703afc99ac09f2071d71b82cb9
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-07 21:29:29 +0300

    feat: theme provider added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Albert_Sans } from "next/font/google";
 import { Footer } from "@/components/layout/Footer";
 import { Session } from "@/components/provider/Session";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const AlbertSans = Albert_Sans({ subsets: ["latin"] });
 
@@ -14,12 +15,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${AlbertSans.className} flex flex-col justify-between items-center h-screen`}>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${AlbertSans.className} flex flex-col justify-between items-center h-screen selection:bg-primary selection:text-black font-semibold `}>
         <Session>
-          <Header />
-          {children}
-          <Footer />
+          <ThemeProvider attribute="class" enableSystem>
+            <Header />
+            {children}
+            <Footer />
+          </ThemeProvider>
         </Session>
       </body>
     </html>
