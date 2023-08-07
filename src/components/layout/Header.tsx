d3a85ed987bc6ddc94b0ef1c79aa60782fb19da8
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-07 12:34:52 +0300

    fix: w-full changed w-auto

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ export async function Header() {
       <nav className="w-full flex justify-between items-center px-[8px] md:px-[3rem] py-[2.75rem] text-black">
         <Link
           href="/"
-          className=" text-2xl font-extrabold text-[2.3rem] -tracking-[0.055em]  lg:text-[2.813rem] w-full  padding lg:font-bold ">
+          className=" text-2xl font-extrabold text-[2.3rem] -tracking-[0.055em]  lg:text-[2.813rem] w-auto  padding lg:font-bold ">
           PLAYLIST AI
         </Link>
         {session === null ? <Login>Sign in with Spotify</Login> : <ProfileCard />}
