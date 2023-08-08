f7be95847c17989144f0b6e2d943e27954d4f88d
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-08 12:00:40 +0300

    fix: profile card has become more organized

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -6,13 +6,10 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { signOut, useSession } from "next-auth/react";
 import { Label } from "./ui/label";
 
@@ -33,21 +30,19 @@ export function ProfileCard() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
-          className="group h-[3.2rem] min-w-[8.72rem] cursor-pointer   gap-1.5 lg:gap-2 lg:min-w-[11.56em] lg:h-[3.5em]  font-semibold border-black 
-          hover:border-black border-solid rounded-[5px] border-[3px] text-[1rem] lg:text-[1.125rem] flex justify-center lg:justify-between items-center bg-opposite text-black hover:bg-black hover:text-secondary transition-all ease-in-out -tracking-wider text-secondary"
-          variant="outline">
-          <Label className="text-md lg:text-xl cursor-pointer   font-semibold">{data?.user.id}</Label>
-          <Avatar className=" hidden md:flex lg:w-[55px] w-10 h-10 lg:h-[55px] border-[3px] border-black  rounded-full">
-            <AvatarImage src={data?.user.image} />
-            <AvatarFallback>PL</AvatarFallback>
-          </Avatar>
+          className="outline-none  dark:bg-[#11d43c] hover:dark:bg-green-500 group h-[3.2rem] min-w-[8.72rem] cursor-pointer md:min-w-[10rem] lg:min-w-[13rem] lg:h-[4rem]  font-semibold  text-[1rem] lg:text-[1.125rem] items-center transition-all ease-in-out -tracking-wider text-white flex justify-center"
+          variant="default">
+          <Label className="text-md lg:text-xl cursor-pointer font-semibold">{data?.user.id}</Label>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-[12.72rem] lg:w-[13rem] border-[3px] border-black bg-opposite text-white">
-        <DropdownMenuLabel className="mx-2">Profile Settings</DropdownMenuLabel>
-        <DropdownMenuSeparator className="border-1 mx-2 mb-2 border-black " />
-        <DropdownMenuRadioGroup value={position?.logout} onValueChange={() => setPosition({ logout: "logout" })}>
-          <DropdownMenuRadioItem className="cursor-pointer pl-[10px] mx-2" value="logout">
+      <DropdownMenuContent className=" border-red-500 hover:border-red-500 p-0 min-w-[8.72rem] h-[3.2rem] lg:h-auto md:w-[9.74rem] lg:w-[13rem] border-[3px]  bg-opposite">
+        <DropdownMenuRadioGroup
+          className="h-[3.2rem]  "
+          value={position?.logout}
+          onValueChange={() => setPosition({ logout: "logout" })}>
+          <DropdownMenuRadioItem
+            className=" focus:bg-destructive bg-destructive/80   p-1 w-full h-[100%] pb-2 lg:pb-0.5 text-base md:text-xl   cursor-pointer flex justify-center items-center text-primary-foreground focus:text-white  "
+            value="logout">
             Logout
           </DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
