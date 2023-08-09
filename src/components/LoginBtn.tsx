37ad7d41f26db6603e6df18bdd44e921bf5935f3
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-09 14:11:12 +0300

    feat: callbackUrl changed endpoint

--- src/components/LoginBtn.tsx
+++ src/components/LoginBtn.tsx
@@ -6,7 +6,7 @@ export function Login({ children }: { children: React.ReactNode }) {
   return (
     <Button
       variant={"outline"}
-      onClick={() => signIn("spotify", { callbackUrl: "/playlist" })}
+      onClick={() => signIn("spotify", { callbackUrl: "/generate" })}
       className="h-[3rem] w-[9.72rem] p-1 lg:w-[11.56em] lg:h-[3.5em]  font-semibold text-[1rem] lg:text-[1.125rem] flex justify-center items-center transition-all ease-in-out -tracking-wider border-[3px] border-secondary-foreground hover:bg-secondary-foreground hover:text-white hover:dark:text-secondary ">
       {children}
     </Button>
