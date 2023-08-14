145a2041a3fd4ef25ebb8b19a3cec72e158a83b7
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-14 21:56:14 +0300

    feat: added animate-down-to-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,10 +61,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "down-to-up": {
+          from: { transform: "translateY(1.5rem)", opacity: 0 },
+          to: { transform: "translateY(0)", opacity: 1 },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "down-to-up": "down-to-up 1.2s ease ",
       },
     },
   },
