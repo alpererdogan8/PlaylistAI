8db03343a336ee0343bda29ce3bebd1f82ee7474
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-16 11:29:22 +0300

    add: added disable feature to disable processing without a response from prompt

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -17,27 +17,37 @@ export function PlaylistCard() {
     description: "Songs you like edited by gpt",
     public: false,
   });
+  const [isDisable, setIsDisable] = useState<boolean>(true);
 
   return (
     <Card className="animate-down-to-up flex flex-col sm:flex-row w-11/12 md:w-8/12  max-w-[900px] m-8 drop-shadow-xl h-auto p-2 border-2 border-slate-700  dark:border-border">
-      <div className="bg-primary rounded-lg  text-[#23FF53] w-full h-[13.25rem] sm:w-[15.63rem] shrink-0 md:h-[15.25rem] pt-5 pl-5 text-4xl">
+      <div
+        className={` ${
+          isDisable ? "bg-slate-500 text-black/60" : "bg-primary text-[#23FF53]"
+        } rounded-lg w-full h-[13.25rem] sm:w-[15.63rem] shrink-0 md:h-[15.25rem] pt-5 pl-5 text-4xl`}>
         Playlist AI
       </div>
       <div className="p-2 w-full flex flex-col justify-between">
         <CardHeader className="flex p-0 flex-row justify-between">
           <div className="mb-10 md:mb-0">
-            <CardTitle className="text-4xl  ">
+            <CardTitle className="text-4xl">
               <TextareaAutosize
+                disabled={isDisable}
                 maxRows={4}
-                className=" w-11/12  lg:w-full lg:max-w-2xl  bg-transparent resize-none scroll overflow-hidden outline-none hover:underline underline-offset-2"
+                className={`${
+                  isDisable ? "" : "hover:underline "
+                } w-11/12 lg:w-full lg:max-w-2xl  bg-transparent resize-none scroll overflow-hidden outline-none underline-offset-2`}
                 value={playlistInfo.name}
                 onChange={(e: any) => setPlaylistInfo({ ...playlistInfo, name: e.target.value })}
               />
             </CardTitle>
             <CardDescription className="text-xl">
               <TextareaAutosize
+                disabled={isDisable}
                 maxRows={4}
-                className="w-11/12 lg:w-full lg:max-w-2xl  bg-transparent resize-none scroll overflow-hidden outline-none hover:underline underline-offset-2"
+                className={`${
+                  isDisable ? "" : "hover:underline "
+                }  w-11/12 lg:w-full lg:max-w-2xl  bg-transparent resize-none scroll overflow-hidden outline-none underline-offset-2`}
                 value={playlistInfo.description}
                 onChange={(e: any) => setPlaylistInfo({ ...playlistInfo, description: e.target.value })}
               />
@@ -46,6 +56,7 @@ export function PlaylistCard() {
           <div className="hidden lg:flex justify-between space-x-2">
             <span>Public</span>
             <Switch
+              disabled={isDisable}
               onCheckedChange={() => setPlaylistInfo({ ...playlistInfo, public: !playlistInfo.public })}
               className="data-[state=checked]:bg-[#23FF53] bg-[#23FF53]"
               checked={playlistInfo.public}
@@ -57,12 +68,15 @@ export function PlaylistCard() {
           <div className="flex lg:hidden justify-between md:justify-end space-x-2">
             <span>Public</span>
             <Switch
+              disabled={isDisable}
               onCheckedChange={(e: any) => setPlaylistInfo({ ...playlistInfo, public: !playlistInfo.public })}
               className="data-[state=checked]:bg-[#23FF53] mx-3 bg-[#23FF53]"
               checked={playlistInfo.public}
             />
           </div>
-          <Button className=" bg-black dark:text-black dark:bg-white dark:hover:bg-primary dark:hover:text-white">
+          <Button
+            disabled={isDisable}
+            className=" bg-black dark:text-black dark:bg-white dark:hover:bg-primary dark:hover:text-white">
             Create Playlist
           </Button>
         </CardFooter>
