763411ea8c98890aac0be97e0167e08bf6f38b12
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-16 11:49:00 +0300

    feat: added general component listing music

---
+++ src/components/GeneratedList.tsx
@@ -0,0 +1,47 @@
+import { MusicItem } from "./MusicItem";
+
+export function GeneratedList() {
+  return (
+    <section className="mb-36 w-full flex flex-col items-center">
+      <h1 className="text-4xl underline underline-offset-8 mb-10">Generated List</h1>
+      <section className="flex flex-wrap justify-center gap-2 w-full">
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+      </section>
+    </section>
+  );
+}
