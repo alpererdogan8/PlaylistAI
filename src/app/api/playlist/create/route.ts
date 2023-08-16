13a87655a90253133f2e766b1baefe15072f7311
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-16 11:51:41 +0300

    feat(api): added post endpoint with users playlist

--- src/app/api/playlist/create/route.ts
+++ src/app/api/playlist/create/route.ts
@@ -1,15 +1,18 @@
 import { IPlaylistCreate } from "@/lib/types/types";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function POST(request: Request) {
-  const req: IPlaylistCreate = await request.json();
-  const res = await fetch(`https://api.spotify.com/v1/users/${req?.session?.id}/playlists`, {
+  const session = await getServerSession(authOptions);
+  const requestBody: IPlaylistCreate = await request.json();
+  const res = await fetch(`https://api.spotify.com/v1/users/${session?.user.id}/playlists`, {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${req?.session?.access_token}`,
+      Authorization: `Bearer ${session?.access_token}`,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(req.body),
+    body: JSON.stringify(requestBody),
   });
 
   return NextResponse.json(await res.json());
