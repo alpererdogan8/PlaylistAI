600a985faee7079586e945344ae070d913170489
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-17 22:12:25 +0300

    fix: inside button null problem solved with if condision

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -17,7 +17,9 @@ export function ThemeButton() {
         variant={"outline"}
         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
         className="w-[16em] h-[3rem] md:w-[10.563rem] md:h-[3.563rem] transition-all border-2 font-bold text-md  md:text-lg tracking-[-5.5%] rounded-[5px] bg-black text-primary-foreground">
-        {currentTheme?.charAt(0).toUpperCase() + currentTheme?.slice(1)!}
+        {
+          currentTheme ? currentTheme.charAt(0).toUpperCase() + currentTheme.slice(1) : "Theme" // Replace with appropriate fallback text
+        }
       </Button>
     </>
   );
