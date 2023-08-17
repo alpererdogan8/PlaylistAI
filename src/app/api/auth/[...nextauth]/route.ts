5f220a379122d366377f72817907020b9474a032
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-17 22:39:55 +0300

    add: erroneously posted commit deleted and added again

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,43 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions, TokenSet } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import SpotifyProvider from "next-auth/providers/spotify";
+
+async function refreshAccessToken(token: JWT) {
+  try {
+    const url = "https://accounts.spotify.com/api/token";
+    const authHeader = process.env.SPOTIFY_ID + ":" + process.env.SPOTIFY_SECRET;
+    const based = Buffer.from(authHeader).toString("base64");
+    const body = "grant_type=refresh_token&refresh_token=" + token.refresh_token;
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Basic " + based,
+      },
+      body: body,
+      method: "POST",
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      access_token: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refresh_token: refreshedTokens.refresh_token ?? token.refresh_token,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -18,18 +56,27 @@ export const authOptions: AuthOptions = {
   },
 
   callbacks: {
-    async jwt({ token, account, user }) {
+    async jwt({ token, account }) {
       if (account) {
+        token.id = account.providerAccountId;
+        token.expires_at = account.expires_at;
         token.access_token = account.access_token;
+        token.accessTokenExpires = Date.now() + account.expires_at!;
+
+        token.refresh_token = account.refresh_token;
       }
-      if (user) {
-        token.id = user.id;
+
+      if (Date.now() < token.accessTokenExpires!) {
+        return token;
       }
-      return token;
+
+      return refreshAccessToken(token);
     },
     async session({ session, token }) {
-      session.user.id = token.id;
+      session.user.id = token.id!;
       session.access_token = token.access_token;
+      session.refresh_token = token.refresh_token;
+      session.expires = token.accessTokenExpires!;
       return session;
     },
   },
