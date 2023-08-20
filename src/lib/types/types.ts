91db4f244c68dd16c91d1b15183061d2956e9252
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-20 13:20:55 +0300

    chore: reorganized interfaces, removed some values

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,20 +1,12 @@
 export interface IPlaylistCreate {
-  body: {
-    name: string;
-    description: string;
-    public: boolean;
-  };
-  session: {
-    id?: string;
-    access_token?: string;
-  };
+  name: string;
+  description: string;
+  public: boolean;
 }
-
 export interface ICard {
   header: string;
   image: any;
   alt: string;
-
   imgHeight: number;
   imgWidth: number;
 }
