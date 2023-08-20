4e8f2ecba4f5cf6c9593a026ed92edc685e79e80
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-20 14:41:25 +0300

    feat: add page

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,20 +1,65 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { GeneratedList } from "@/components/GeneratedList";
+import { PlaylistCard } from "@/components/PlaylistCard";
+import { Prompt } from "@/components/Prompt";
+import { Skeleton } from "@/components/Skeleton";
+import { Button } from "@/components/ui/button";
+import { signIn, useSession } from "next-auth/react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
+import { refreshAccessToken } from "../api/auth/[...nextauth]/route";
 
-const getPlaylist = async ({ token }: { token: string }) => {
-  const res = await fetch(`https://api.spotify.com/v1/me/tracks?limit=1&offset=0`, {
-    method: "GET",
+type IGetPlaylist = Record<string, string | number>;
+
+const getPlaylist = async ({ token, limit, offset }: IGetPlaylist) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/tracks/me?limit=${limit}&offset=${offset}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createPlaylistFetch = async (token: string) => {
+  const dene = await fetch(`http://localhost:3000/api/playlist/create`, {
+    method: "POST",
     headers: {
-      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      name: "alper'in playlisti",
+      description: "erdogan'ın playlisti",
+      public: false,
+    }),
   });
-  return res.json();
+  const result = await dene.json();
+  return result;
+};
+const addTrackFetch = async (playlistId: string) => {
+  const dene = await fetch(`http://localhost:3000/api/playlist/${playlistId}/track`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      uris: ["spotify:track:1iaCXdnlc1QPpktPidJm9h"],
+      position: 0,
+    }),
+  });
+  const result = await dene.json();
+  return result;
 };
 
 const Page = () => {
+  const [deneFetch, setDeneFetch] = useState([]);
   const { data, status, update } = useSession({
     required: true,
     onUnauthenticated() {
@@ -22,22 +67,52 @@ const Page = () => {
     },
   });
 
-  const [myPlaylist, setMyPlaylist] = useState([]);
+  useEffect(() => {
+    if (data?.error === "RefreshAccessTokenError") {
+      signIn("spotify");
+    }
+  }, [data]);
+
+  async function createPlaylist() {
+    const dene = await createPlaylistFetch(data?.access_token!);
+    setDeneFetch(dene);
+  }
+
+  async function addTrack() {
+    const dene = await addTrackFetch("6ukD7s3DB6rsna5CZGJEvG");
+    setDeneFetch(dene);
+  }
+
+  const [myPlaylist, setMyPlaylist] = useState<string[]>([]);
 
   useEffect(() => {
     async function fetchPlaylist() {
-      const res = await getPlaylist({ token: data?.access_token! });
-      setMyPlaylist(await res);
+      const res = await getPlaylist({ token: data?.access_token!, limit: 10, offset: 0 });
+
+      setMyPlaylist([...myPlaylist, res]);
     }
     fetchPlaylist();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data?.access_token]);
 
   return (
-    <>
-      <pre className="whitespace-pre-wrap w-7/12">{JSON.stringify({ myPlaylist }, null, 2)}</pre>
-      <pre className="whitespace-pre-wrap">{JSON.stringify({ data: data?.access_token }, null, 2)}</pre>
-      <hr />
-    </>
+    // min-h-[calc(100dvh+185px)] h-auto
+    <div className="flex flex-col w-full items-center">
+      {/* <Suspense fallback={<Skeleton />}>
+        <Prompt />
+        <PlaylistCard />
+        <GeneratedList />
+      </Suspense> */}
+      {/* <pre className="whitespace-pre-wrap">{JSON.stringify({ status }, null, 2)}</pre> */}
+      {/* <pre className="whitespace-pre-wrap">{JSON.stringify({ myPlaylist }, null, 2)}</pre> */}
+      <pre className="whitespace-pre-wrap">{JSON.stringify({ data }, null, 2)}</pre>
+      {/* <Button onClick={createPlaylist}>Create Playlist</Button>
+      <Button onClick={addTrack}>Add Music</Button>
+
+      <Link href="spotify://playlist/7q137No7rNy7TJZ6sacDiT">Go to Spotify</Link>
+      <pre className="whitespace-pre-wrap">{JSON.stringify(deneFetch)}</pre>
+      <hr /> */}
+    </div>
   );
 };
 
