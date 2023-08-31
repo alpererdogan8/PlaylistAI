f31f9c6c2fe16210e5df71146dfc3f9dc32b4145
Alper Erdogan <alpererdogan8@gmail.com>
2023-08-31 19:20:34 +0300

    chore: prompt examples were deleted from the lines of this file because they were moved under the utils folder

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react";
+import { FormEvent, MutableRefObject, RefAttributes, useEffect, useRef, useState } from "react";
 import { PlaylistCard } from "./PlaylistCard";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { exampleList } from "@/lib/prompt_examples";
 
 export function Prompt() {
-  const exampleList: string[] = [
-    "Pop",
-    "Rock",
-    "Hip-hop",
-    "Jazz",
-    "Blues",
-    "Country",
-    "Electronic",
-    "Classical",
-    "R&B",
-    "Reggae",
-    "Metal",
-    "Folk",
-    "Funk",
-    "Punk",
-    "Indie",
-    "Dance",
-    "Soul",
-    "Gospel",
-    "Salsa",
-    "Dubstep",
-  ];
   const [examplePrompts, setExamplePrompts] = useState<string>("Create a rap playlist");
   useEffect(() => {
     const exampleLoop = setInterval(() => {
@@ -36,17 +15,33 @@ export function Prompt() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [examplePrompts]);
+  const ref = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (ref.current) {
+      console.log(ref.current.prompt?.value);
+    }
+  };
+
   return (
-    <div className="w-11/12 md:w-8/12  max-w-[900px] m-2 animate-down-to-up h-auto flex items-center justify-end ">
+    <form
+      ref={ref}
+      onSubmit={handleSubmit}
+      className="w-11/12 md:w-8/12  max-w-[900px] m-2 animate-down-to-up h-auto flex items-center justify-end ">
       <Input
         type="text"
+        name="prompt"
         className="w-full h-[53px] border-slate-700 border-2 focus:border-2 focus:drop-shadow-xl focus:border-slate-700 focus:outline-none dark:focus:border-primary dark:focus:border-2 dark:transition-none transition-all text-secondary-foreground font-semibold text-lg -tracking-wider rounded-[0.625rem]"
         placeholder={examplePrompts}
       />
-      <Button className="absolute mr-[7px] w-[125px] rounded-[0.438rem] bg-black dark:text-black dark:bg-white dark:hover:bg-[#11d43c] leading-3 dark:hover:text-white">
+      <Button
+        type="submit"
+        disabled={false}
+        className="absolute mr-[7px] w-[125px] rounded-[0.438rem] bg-black dark:text-black dark:bg-white dark:hover:bg-[#11d43c] leading-3 dark:hover:text-white">
         Generate
       </Button>
-    </div>
+    </form>
   );
 }
 
