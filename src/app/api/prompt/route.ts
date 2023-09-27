d2dadbec41545f2bbff8f9e6582bb0a4067cecaf
Alper Erdogan <alpererdogan8@gmail.com>
2023-09-27 09:29:39 +0300

    feat: updated prompts, updated headers, updated LLM model and refactored chat completion settings

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -1,5 +1,5 @@
 import { Configuration, OpenAIApi } from "openai-edge";
-import { OpenAIStream, StreamingTextResponse, streamToResponse } from "ai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -7,14 +7,38 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 export const runtime = "edge";
-const systemPrompt = `Translate from English to Turkish`;
-export async function POST(req: Request) {
-  const { messages } = await req.json();
-  const combineMessage = [...messages, { role: "system", content: systemPrompt }];
+const systemPrompt = (text: string) => {
+  return `I want you to act like a music classifier, I give you a list of music between three hyphens (csv), I give you a hint on how to organize it and it is limited to three backslashes.
 
+  Rules you need to know:
+  
+-When answering the question, the answer *must* be in square brackets, for example "['music1','music2','music3']"
+-You only categorize music and nothing else, so any false request other than playlists and music should be answered *I categorize music* and it should be returned as *string*
+  -When categorizing music, look for criteria such as music genre, language, artist name, etc. *if you know them*. If it does not meet most of these criteria and you are *not sure*, please do not include the music in the list
+-Return your request response with only *music name* in the array.
+---${text}---`;
+};
+
+export async function POST(request: Request) {
+  const { token, messages } = await request.json();
+
+  const res = await fetch("http://localhost:3000/api/tracks/me?csv=true", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token: token }),
+  });
+  const playlist = await res.json();
+  const content = systemPrompt(playlist);
+
+  const combineMessage = [...messages, { role: "system", content }];
   const response = await openai.createChatCompletion({
-    max_tokens: 150,
-    model: "gpt-3.5-turbo-16k",
+    max_tokens: 1000,
+    frequency_penalty: 0.8,
+    presence_penalty: 0.8,
+    temperature: 0.7,
+    model: "gpt-3.5-turbo-16k-0613",
     messages: combineMessage.map((message: any) => ({
       content: message.content,
       role: message.role,
@@ -22,6 +46,11 @@ export async function POST(req: Request) {
     stream: true,
   });
   const stream = await OpenAIStream(response);
-
-  return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(stream, {
+    headers: {
+      "X-RateLimit-Remaining": request.headers.get("X-RateLimit-Remaining")!,
+      "X-RateLimit-Limit": request.headers.get("X-RateLimit-Limit")!,
+      "X-RateLimit-Reset": request.headers.get("X-RateLimit-Reset")!,
+    },
+  });
 }
