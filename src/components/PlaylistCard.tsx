8de143aadc4e636ea0bb64d27ab36db5c32a4e9d
Alper Erdogan <alpererdogan8@gmail.com>
2023-10-03 18:17:09 +0300

    feat(components): Update PlaylistCard component with dynamic button and context usage

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -2,28 +2,25 @@
 import { useState } from "react";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { Switch } from "./ui/switch";
-import { Button } from "./ui/button";
 import TextareaAutosize from "react-textarea-autosize";
-
-type TPlaylistInfo = {
-  name?: string;
-  description?: string;
-  public?: true | false;
-};
+import useContextApi from "@/context/ContextApi";
+import { IPlaylistCreate } from "@/lib/types/types";
+import Button from "./Button";
 
 export function PlaylistCard() {
-  const [playlistInfo, setPlaylistInfo] = useState<TPlaylistInfo>({
+  const { createPlaylist, state } = useContextApi();
+
+  const [playlistInfo, setPlaylistInfo] = useState<IPlaylistCreate>({
     name: "My AI Mix",
     description: "Songs you like edited by gpt",
-    public: false,
+    isPublic: false,
   });
-  const [isDisable, setIsDisable] = useState<boolean>(true);
 
   return (
     <Card className="animate-down-to-up flex flex-col sm:flex-row w-11/12 md:w-8/12  max-w-[900px] m-8 drop-shadow-xl h-auto p-2 border-2 border-slate-700  dark:border-border">
       <div
         className={` ${
-          isDisable ? "bg-slate-500 text-black/60" : "bg-primary text-[#23FF53]"
+          !state.isPromptAvailable ? "bg-slate-500 text-black/60" : "bg-primary text-[#23FF53]"
         } rounded-lg w-full h-[13.25rem] sm:w-[15.63rem] shrink-0 md:h-[15.25rem] pt-5 pl-5 text-4xl`}>
         Playlist AI
       </div>
@@ -32,10 +29,10 @@ export function PlaylistCard() {
           <div className="mb-10 md:mb-0">
             <CardTitle className="text-4xl">
               <TextareaAutosize
-                disabled={isDisable}
+                disabled={!state.isPromptAvailable}
                 maxRows={4}
                 className={`${
-                  isDisable ? "" : "hover:underline "
+                  !state.isPromptAvailable ? "" : "hover:underline "
                 } w-11/12 lg:w-full lg:max-w-2xl  bg-transparent resize-none scroll overflow-hidden outline-none underline-offset-2`}
                 value={playlistInfo.name}
                 onChange={(e: any) => setPlaylistInfo({ ...playlistInfo, name: e.target.value })}
@@ -43,10 +40,10 @@ export function PlaylistCard() {
             </CardTitle>
             <CardDescription className="text-xl">
               <TextareaAutosize
-                disabled={isDisable}
+                disabled={!state.isPromptAvailable}
                 maxRows={4}
                 className={`${
-                  isDisable ? "" : "hover:underline "
+                  !state.isPromptAvailable ? "" : "hover:underline "
                 }  w-11/12 lg:w-full lg:max-w-2xl  bg-transparent resize-none scroll overflow-hidden outline-none underline-offset-2`}
                 value={playlistInfo.description}
                 onChange={(e: any) => setPlaylistInfo({ ...playlistInfo, description: e.target.value })}
@@ -56,10 +53,10 @@ export function PlaylistCard() {
           <div className="hidden lg:flex justify-between space-x-2">
             <span>Public</span>
             <Switch
-              disabled={isDisable}
-              onCheckedChange={() => setPlaylistInfo({ ...playlistInfo, public: !playlistInfo.public })}
+              disabled={!state.isPromptAvailable}
+              onCheckedChange={() => setPlaylistInfo({ ...playlistInfo, isPublic: !playlistInfo.isPublic })}
               className="data-[state=checked]:bg-[#23FF53] bg-[#23FF53]"
-              checked={playlistInfo.public}
+              checked={playlistInfo.isPublic}
             />
           </div>
         </CardHeader>
@@ -68,15 +65,18 @@ export function PlaylistCard() {
           <div className="flex lg:hidden justify-between md:justify-end space-x-2">
             <span>Public</span>
             <Switch
-              disabled={isDisable}
-              onCheckedChange={(e: any) => setPlaylistInfo({ ...playlistInfo, public: !playlistInfo.public })}
+              disabled={!state.isPromptAvailable}
+              onCheckedChange={(e: any) => setPlaylistInfo({ ...playlistInfo, isPublic: !playlistInfo.isPublic })}
               className="data-[state=checked]:bg-[#23FF53] mx-3 bg-[#23FF53]"
-              checked={playlistInfo.public}
+              checked={playlistInfo.isPublic}
             />
           </div>
           <Button
-            disabled={isDisable}
-            className=" bg-black dark:text-black dark:bg-white dark:hover:bg-primary dark:hover:text-white">
+            type="button"
+            isLoading={state.loading}
+            onClick={() => createPlaylist(playlistInfo)}
+            disabled={!state.isPromptAvailable}
+            className="bg-black   dark:text-black dark:bg-white dark:hover:bg-primary dark:hover:text-white">
             Create Playlist
           </Button>
         </CardFooter>
