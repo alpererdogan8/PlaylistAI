dc86795a00fc54e3e69b159d29d563a1ef5f3a50
Alper Erdogan <alpererdogan8@gmail.com>
2023-10-03 18:32:29 +0300

    chore(types): Update interface definitions and add context API types

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,7 +1,7 @@
 export interface IPlaylistCreate {
   name: string;
   description: string;
-  public: boolean;
+  isPublic: boolean;
 }
 export interface ICard {
   header: string;
@@ -10,3 +10,24 @@ export interface ICard {
   imgHeight: number;
   imgWidth: number;
 }
+export interface IGetPlaylist {
+  limit?: number;
+  offset?: number;
+  csv?: boolean;
+}
+
+export interface IContextApi {
+  getPlaylists: ({ limit, offset }: IGetPlaylist) => Promise<any>;
+  createPlaylist: ({ name, description, isPublic }: IPlaylistCreate) => Promise<any>;
+  addToPlaylist: ({ playlistId, uris }: { playlistId: string; uris: string[] }) => Promise<any>;
+  state: TYPE_STATE;
+  dispatch: React.Dispatch<TYPE_ACTION>;
+}
+export type TYPE_ACTION = { type: "PENDING" | "SUCCESS" | "FAILED"; payload?: { data: any; isTrue?: boolean } };
+
+export type TYPE_STATE = {
+  loading: boolean;
+  data: object[];
+  isPromptAvailable: boolean;
+  error: object;
+};
