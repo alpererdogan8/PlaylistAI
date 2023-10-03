b1854c2ef069ab3bf642335f55160ab003793e1c
Alper Erdogan <alpererdogan8@gmail.com>
2023-10-03 18:13:54 +0300

    feat(components): Update GeneratedList component with dynamic MusicItem rendering

--- src/components/GeneratedList.tsx
+++ src/components/GeneratedList.tsx
@@ -1,46 +1,44 @@
+"use client";
+import useContextApi from "@/context/ContextApi";
 import { MusicItem } from "./MusicItem";
+import { useState } from "react";
+import Loading from "./Loading";
 
 export function GeneratedList() {
+  const [playingIndex, setPlayingIndex] = useState<number | null>(null);
+  const { state } = useContextApi();
+
+  const handlePlay = (index: number) => {
+    setPlayingIndex(index);
+  };
+  const handlePause = () => {
+    setPlayingIndex(null);
+  };
+
   return (
-    <section className="mb-36 w-full flex flex-col items-center">
+    <section className={`${state.data.length > 0 ? "flex" : "hidden"} mb-36 w-full  flex-col items-center`}>
       <h1 className="text-4xl underline underline-offset-8 mb-10">Generated List</h1>
       <section className="flex flex-wrap justify-center gap-2 w-full">
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
-        <MusicItem />
+        {state.data.length > 0 ? (
+          state.data?.map((item: any, index: any) => {
+            return (
+              <MusicItem
+                music={item.music}
+                artist={item.artist}
+                image={item.image}
+                previewUrl={item.previewUrl}
+                uri={item.uri}
+                key={index}
+                index={index}
+                isPlaying={index === playingIndex}
+                onPlay={() => handlePlay(index)}
+                onPause={handlePause}
+              />
+            );
+          })
+        ) : (
+          <Loading />
+        )}
       </section>
     </section>
   );
